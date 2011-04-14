dd6e47dcdce06afe25491c44c5f4aee3fb08f966
Tejaswi Tenneti <tejaswit@stanford.edu>
2011-04-13 19:10:01 -0700

    Verb class

--- src/senna/Verb.java
+++ src/senna/Verb.java
@@ -1,10 +1,13 @@
 package senna;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Verb {
 	
 	public String text;
 	public HashMap<String, String> argumentToText = new HashMap<String, String>();
+	public HashMap<String, List<String>> argumentToNPs = new HashMap<String, List<String>>();
 	
 }
