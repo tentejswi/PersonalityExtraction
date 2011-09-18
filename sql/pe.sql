ae517cfcdbbd9c1c844e8c4c74bdfa84d939c896
semanticvoid <anand@semanticvoid.com>
2011-09-18 15:55:45 -0700

    handle source identifier

--- sql/pe.sql
+++ sql/pe.sql
@@ -5,3 +5,5 @@ USE pe;
 CREATE TABLE user_queue(handle VARCHAR(255), done TINYINT(1) DEFAULT 0, updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY(handle), INDEX doneindex (done));
 
 CREATE TABLE user_interests(handle VARCHAR(255), json MEDIUMTEXT NOT NULL, updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY(handle));
+
+alter table user_queue add column type varchar(1) not null after handle;
