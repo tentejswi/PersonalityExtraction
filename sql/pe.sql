4a44e243b3265f65cb0c4a7dab377caa674dfab3
semanticvoid <anand@semanticvoid.com>
2011-09-18 19:29:45 -0700

    increased handle size for FB access tokens

--- sql/pe.sql
+++ sql/pe.sql
@@ -7,3 +7,5 @@ CREATE TABLE user_queue(handle VARCHAR(255), done TINYINT(1) DEFAULT 0, updated 
 CREATE TABLE user_interests(handle VARCHAR(255), json MEDIUMTEXT NOT NULL, updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY(handle));
 
 alter table user_queue add column type varchar(1) not null after handle;
+
+ alter table user_queue modify column handle varchar(600) not null;
