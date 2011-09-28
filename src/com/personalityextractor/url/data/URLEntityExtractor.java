60b67f8de7785521a886fee9cc0f7c8a7dd580a6
Tejaswi Tenneti <tejaswit@stanford.edu>
2011-09-27 23:26:20 -0700

    URLEntityExtractor

--- src/com/personalityextractor/url/data/URLEntityExtractor.java
+++ src/com/personalityextractor/url/data/URLEntityExtractor.java
@@ -16,9 +16,20 @@ public class URLEntityExtractor {
 
 	ViterbiResolver vr = new ViterbiResolver();
 	
-	public List<String> extractEntitiesinTitle(String title) {
-		IEntityExtractor extractor = EntityExtractFactory.produceExtractor(Extracter.SENNANOUNPHRASE);
-		return extractor.extract(title);
+	public static List<String> extractEntitiesinTitle(String urlStr) {
+		ArrayList<String> entities = new ArrayList<String>();
+		String urlContent = URLContent.fetchURLContent(urlStr);
+		if(urlContent==null)
+			return null;
+		String title = URLContent.fetchTitleString(urlContent);
+		IEntityExtractor extractor = EntityExtractFactory.produceExtractor(Extracter.NOUNPHRASE);
+		if(title==null)
+			return null;
+		Tweet t = new Tweet(title);
+		for (String sentence : t.getSentences()) {
+			entities.addAll(extractor.extract(sentence));
+		}
+		return entities;
 	}
 	
 	public List<WikipediaEntity> resolveEntitiesinTitle(List<String> entities){
@@ -41,23 +52,7 @@ public class URLEntityExtractor {
 	
 	public static void main(String[] args) {
 		URLEntityExtractor uee = new URLEntityExtractor();
-		URLContent uc = new URLContent();
-		List<String> testUrls = uee.readLinesinFile(args[0]);
-		for (String urlStr : testUrls) {
-			String urlContent = URLContent.fetchURLContent(urlStr);
-			String title = URLContent.fetchTitleString(urlContent);
-			System.out.println("title: "+ title);
-			Tweet t = new Tweet(title);
-			for (String sentence : t.getSentences()) {
-				List<String> entities = uee.extractEntitiesinTitle(sentence);
-				System.out.println("entities: "+entities);
-				List<WikipediaEntity> wikiEntities = uee.resolveEntitiesinTitle(entities);
-				for (WikipediaEntity we : wikiEntities) {
-					System.out.println(we.getText() + " - "+ we.getWikiminerID());
-				}
-			}
-			System.out.println("\n");
-		}
+		uee.extractEntitiesinTitle("http://berke");
 	}
 		
 }
